6333acb33f65708a33171d2da6d970680a0c37ab
KomalGupta1137 <121231377+KomalGupta1137@users.noreply.github.com>
2023-05-11 15:55:38 +0530

    Create conditionalPrint.py

---
+++ conditionalPrint.py
@@ -0,0 +1,17 @@
+//Make a program that should do the following thing from 1 to 100.
+
+//1.
+
+//Numbers that are divisible by 3, you have to print "K".
+
+//2.
+
+//Numbers that are divisible by 7 so that "omal" is printed.
+
+//3.
+
+//Numbers that are divisible by both 3 and 7, print "Komal" there.
+
+//4.
+
+//If the number does not come in the above three cases then print the number only.
