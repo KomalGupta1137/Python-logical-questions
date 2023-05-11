37332d180243528036aa6cd7dbb70f9a099c6a8f
KomalGupta1137 <121231377+KomalGupta1137@users.noreply.github.com>
2023-05-11 16:05:56 +0530

    Create algorithm.py

---
+++ algorithm.py
@@ -0,0 +1,4 @@
+//Make an algorithm that takes 2 numbers as inputs from the user. Then we will multiply those numbers and print the result. Example :-, if we input 5 and 4 then 20 should be printed.
+
+Note :-
+Don't use * multiply operator to multiply , you can use any other operators like +,-).
