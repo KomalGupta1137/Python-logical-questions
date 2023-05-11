5f051a3d25b56dadf74bc3841f7ee51be1014d17
KomalGupta1137 <121231377+KomalGupta1137@users.noreply.github.com>
2023-05-11 16:09:19 +0530

    Create conditionalCount.py

---
+++ conditionalCount.py
@@ -0,0 +1,3 @@
+//Suppose we have a list of marks and we have to find that how many students have marks less than 50. For doing this we write the code given below.
+
+student_marks = [23, 45, 67, 89, 90, 54, 34, 21, 34, 23, 19, 28, 10, 45, 86, 87, 9]
