f732c464abe70d14f1bec27cd9d46a1851a8930c
KomalGupta1137 <121231377+KomalGupta1137@users.noreply.github.com>
2023-05-11 16:04:50 +0530

    Create printAverage.py

---
+++ printAverage.py
@@ -0,0 +1,1 @@
+//Take input of weights of 11 people and then print their average and then check whether the average weight is a multiple of 5 or not? This means that when you will divide the average weight by 5, the remainder should be 0.
