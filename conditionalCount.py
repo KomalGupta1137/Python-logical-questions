dd77008b3a7cea132368a2155cd4b44a90c058d5
KomalGupta1137 <121231377+KomalGupta1137@users.noreply.github.com>
2023-05-21 19:15:25 +0530

    Update conditionalCount.py

--- conditionalCount.py
+++ conditionalCount.py
@@ -1,3 +1,11 @@
 //Suppose we have a list of marks and we have to find that how many students have marks less than 50. For doing this we write the code given below.
 
-student_marks = [23, 45, 67, 89, 90, 54, 34, 21, 34, 23, 19, 28, 10, 45, 86, 87, 9]
+marks = [23, 45, 67, 89, 90, 54, 34, 21, 34, 23, 19, 28, 10, 45, 86, 87, 9]
+length = len(marks)
+i = 0
+while i < length:
+  if(marks[i] < 50):
+    print(marks[i])
+  i +=1
+
+
